3c2402f97f98b571ae8deeeaa5a109af140560cd
Lena <vicky.peschke@gmail.com>
2013-10-14 20:58:26 +0200

    Pas d'accents! Merci

--- LittleTrees/src/Writer.java
+++ LittleTrees/src/Writer.java
@@ -4,16 +4,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Classe permettant d'écrire dans un fichier de sortie
+ * Classe permettant d'ecrire dans un fichier de sortie
  * 
- * @author Groupe 10, Goéric Huybrechts
+ * @author Group 10, Goeric Huybrechts
  */
 public class Writer {
 	
 	static BufferedWriter output;
 	
 	/**
-	   * Méthode permettant d'ouvrir un fichier de sortie
+	   * Methode permettant d'ouvrir un fichier de sortie
 	   *  
 	   * @pre  -
 	   * @post Ouvre un fichier de sortie
@@ -29,11 +29,11 @@ public class Writer {
 	}
 	
 	/**
-	   * Méthode permettant d'écrire dans le fichier de sortie déjà ouvert
+	   * Methode permettant d'ecrire dans le fichier de sortie deja ouvert
 	   *  
-	   * @pre  Le fichier de sortie "output" doit déjà avoir été ouvert préalablement
+	   * @pre  Le fichier de sortie "output" doit deja avoir ete ouvert prealablement
 	   * @post Ecrit dans un fichier de sortie
-	   * @exception IOException si erreur lors de l'écriture dans le fichier
+	   * @exception IOException si erreur lors de l'ecriture dans le fichier
 	   * 
 	   */ 
 	public static void write(String message) {		
@@ -45,9 +45,9 @@ public class Writer {
 	}
 	
 	/**
-	   * Méthode permettant de fermer le fichier de sortie
+	   * Methode permettant de fermer le fichier de sortie
 	   *  
-	   * @pre  Le fichier de sortie "output" doit déjà avoir été ouvert préalablement
+	   * @pre  Le fichier de sortie "output" doit deja avoir ete ouvert prealablement
 	   * @post Ferme le fichier de sortie
 	   * @exception IOException si erreur lors de la fermeture du fichier
 	   * 
@@ -61,12 +61,12 @@ public class Writer {
 	}
 	
 	/**
-	   * Méthode permettant d'écrire dans un fichier de sortie pas encore ouvert et de refermer celui-ci juste après
+	   * Methode permettant d'ecrire dans un fichier de sortie pas encore ouvert et de refermer celui-ci juste apr��s
 	   *  
 	   * @pre  -
-	   * @post Ouverture du fichier de sortie. Ecriture du String passé en argument dans le fichier de sortie.
+	   * @post Ouverture du fichier de sortie. Ecriture du String passe en argument dans le fichier de sortie.
 	   *  Finalement fermeture du fichier.
-	   * @exception IOException si erreur lors de l'ouverture, de l'écriture ou de la fermeture du fichier
+	   * @exception IOException si erreur lors de l'ouverture, de l'ecriture ou de la fermeture du fichier
 	   * 
 	   */ 
 	public static void write(String outputFile, String message) {		
